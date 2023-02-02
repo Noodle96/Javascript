9af8b741b48a70da0b8fa76c5080c4dfadac8516
Noodle96 <jorgealfredomarino2@gmail.com>
2023-02-02 01:34:31 -0500

    addEventListener click, mousenter, mouseout, dblclick event

--- 14-Eventos/js/01-scripts.js
+++ 14-Eventos/js/01-scripts.js
@@ -1,3 +1,29 @@
+//001- DOMContentLoaded event
+console.log('001----');
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Documento Listo');
 })
+
+// EVENTOS CON EL MOUSE
+console.log('002----');
+const nav = document.querySelector('.navegacion');
+//--------------- CLICK
+// nav.addEventListener('click',()=>{
+//     console.log('click en nav');
+// });
+
+//--------------MOUSEENTER
+// nav.addEventListener('mouseenter',()=>{
+//     console.log('pasando por la navegacion');
+// });
+
+//------------ MOUSEOUT
+// nav.addEventListener('mouseout',()=>{
+//     console.log('saliendo de la navegacion');
+// });
+
+
+//------------- MOUSEENTER
+nav.addEventListener('mouseenter',()=>{
+    console.log('entrando a la navegacion');
+});
