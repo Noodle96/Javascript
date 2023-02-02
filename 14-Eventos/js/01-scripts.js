5fba2c29ccec7cdc56b571d9e538754e2d693cd4
Noodle96 <jorgealfredomarino2@gmail.com>
2023-02-02 01:52:12 -0500

    addEventListener keydown, keyup, blur, copy, paste, cut, input event

--- 14-Eventos/js/01-scripts.js
+++ 14-Eventos/js/01-scripts.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Documento Listo');
 })
 
-// EVENTOS CON EL MOUSE
+// 002-  EVENTOS CON EL MOUSE
 console.log('002----');
 const nav = document.querySelector('.navegacion');
 //--------------- CLICK
@@ -24,6 +24,46 @@ const nav = document.querySelector('.navegacion');
 
 
 //------------- MOUSEENTER
-nav.addEventListener('mouseenter',()=>{
-    console.log('entrando a la navegacion');
+// nav.addEventListener('mouseenter',()=>{
+//     console.log('entrando a la navegacion');
+// });
+
+//003-VENTOS QUE SUCEDEN CON EL TECLADO
+console.log('03---');
+//---------KEYDOWN
+const busqueda = document.querySelector('.busqueda');
+// busqueda.addEventListener('keydown',()=>{
+//     console.log('presionando una tecla .....');
+// });
+
+// -----------KEYUP
+// busqueda.addEventListener('keyup',()=>{
+//     console.log('levantando la tecla .....');
+// });
+
+//------------ BLUR
+// busqueda.addEventListener('blur',()=>{
+//     console.log('click fuera del form .....');
+// });
+
+// ----------COPY
+// busqueda.addEventListener('copy',()=>{
+//     console.log('Se ha copiado el texto del form.....');
+// });
+
+
+//---------PASTE
+// busqueda.addEventListener('paste',()=>{
+//     console.log('Se ha pegado algo de texto en el form.....');
+// });
+
+
+//---------PASTE
+// busqueda.addEventListener('cut',()=>{
+//     console.log('Se ha cortado algo de texto en el form.....');
+// });
+
+// ---------- INPUT
+busqueda.addEventListener('input',(e)=>{
+    console.log(e.target.value);
 });
