8a6b26c25687648afe2ad0166618defc139d479a
Noodle96 <jorgealfredomarino2@gmail.com>
2023-02-17 01:38:43 -0500

    Proyecto Enviar Emails, seleccionando elementos del formulario

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('desde DOMContentLoaded');
+    //seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+
+
+    console.log(inputMensaje);
+});
