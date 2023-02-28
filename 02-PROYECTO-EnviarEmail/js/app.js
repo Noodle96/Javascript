7f6776b167ef02a5add8e8d34e85ef0dd30f49db
Noodle96 <jorgealfredomarino2@gmail.com>
2023-02-28 07:28:59 -0500

    Crear y añadir en el HTML la alerta cuando el campo esta vacio

--- 02-PROYECTO-EnviarEmail/js/app.js
+++ 02-PROYECTO-EnviarEmail/js/app.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
-    console.log('desde DOMContentLoaded');
+    // console.log('desde DOMContentLoaded');
     //seleccionar los elementos de la interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     //EVENTOS IDEALES PARA VALIDAR EL FORMULARIO:
     //event blur cuando abandonas un campo
@@ -12,6 +13,18 @@ document.addEventListener('DOMContentLoaded', function(){
     inputMensaje.addEventListener('blur', validar);
 
     function validar(e){
-        console.log(e.target.value);
+        //validando que el campo no este vacio
+        if(e.target.value.trim() === ''){
+            generarAlertaHTML();
+        }else{
+            console.log('no vacio');
+        }
+    }
+
+    function generarAlertaHTML(){
+        const error = document.createElement('p');
+        error.textContent = 'Hubo un error';
+        error.classList.add('bg-red-700','text-white','p-2','text-center');
+        formulario.appendChild(error);
     }
 });
