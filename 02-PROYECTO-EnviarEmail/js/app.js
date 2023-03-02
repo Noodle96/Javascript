c1a3c48a36e12ca59058f911cf50fba2fe2b72e8
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-02 18:22:49 -0500

    Resetar el formulario

--- 02-PROYECTO-EnviarEmail/js/app.js
+++ 02-PROYECTO-EnviarEmail/js/app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
     const data = {
         email: '',
         asunto: '',
@@ -17,6 +18,14 @@ document.addEventListener('DOMContentLoaded', function(){
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
+    btnReset.addEventListener('click',function(e){
+        e.preventDefault();
+        data.email = '';
+        data.asunto = '';
+        data.mensaje = '';
+        formulario.reset();
+        comprobarData();
+    });
 
     function validar(e){
         //validando que el campo no este vacio
