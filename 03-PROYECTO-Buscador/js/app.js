9c4e93999992d2ff1f2d806e50458934194c3ae6
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-02 23:31:07 -0500

    Generar el select de años con js

--- 03-PROYECTO-Buscador/js/app.js
+++ 03-PROYECTO-Buscador/js/app.js
@@ -4,10 +4,18 @@ const colores = ['#C0C0C0','#808080','#000000','#FF0000','#800000','#FFFF00',
                     '#808000','#00FF00','#008000','#00FFFF','#008080','#0000FF',
                     '#000080','#FF00FF','#800080',
                 ];
+const year = document.querySelector('#year');
+console.log(year);
+const maxYear = new Date().getFullYear();
+const minYear = maxYear - 10;
 
 //EVENTOS
 document.addEventListener('DOMContentLoaded', ()=>{
+    // muestra los auto de la DB (db.js)
     mostrarAutos();
+
+    // llena las option del campo year
+    llenarSelectYear();
 });
 
 
@@ -28,11 +36,11 @@ function mostrarAutos(){
     });
 }
 
-
-// marca: 'BMW',
-// 		modelo: 'Serie 3',
-// 		year: 2020,
-// 		precio: 30000,
-// 		puertas: 4,
-// 		color: 'Blanco',
-// 		transmision: 'automatico'
+function llenarSelectYear(){
+    for(let i_year = maxYear; i_year >=minYear; i_year--){
+        const option = document.createElement('OPTION');
+        option.value = i_year;
+        option.textContent = i_year;
+        year.appendChild(option);
+    }
+}
