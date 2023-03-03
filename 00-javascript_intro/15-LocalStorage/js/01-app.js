e22e4463d259cbbe1ca5d618beff45880f925019
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-03 16:05:24 -0500

    Get data of localStorage

--- 00-javascript_intro/15-LocalStorage/js/01-app.js
+++ 00-javascript_intro/15-LocalStorage/js/01-app.js
@@ -1,7 +1,10 @@
+//001-- setItem()
 //LOCALSTORAGE  store only string
-// to convert to string => (JSON.stringify(object))
-localStorage.setItem('Nick', 'Russell');
+// Convert object to string => JSON.stringify(object)
+// Convert string to Object => JSON.parse(string);
 
+localStorage.setItem('Nick', 'Russell');
+localStorage.setItem('Age',1);
 //STORE ONE OBJECT IN LOCALSTORAGE
 const producto = {
     nombre: 'Vanish x750 FGH',
@@ -17,7 +20,29 @@ console.log(productoString);
 //STORE A ARRAY IN LOCALSTORAGE
 const meses = ['January', 'Febrary', 'March', 'April'];
 const mesesString = JSON.stringify(meses);
-localStorage.setItem('MESEs',mesesString);
+localStorage.setItem('meses',mesesString);
 
 console.log(meses);
 console.log(mesesString);
+
+
+//002-- getItem()
+//OBTENER DATOS DEL LOCALSTORAGFE
+console.log('002---');
+const nick = localStorage.getItem('Nick');
+const age = localStorage.getItem('Age');
+// console.log(localStorage.length);
+// console.log(typeof nick); // string
+// console.log(typeof age); //string
+
+const getProducto = localStorage.getItem('producto');
+const getMeses= localStorage.getItem('meses');
+
+// console.log(typeof getProducto);//string
+const productoParseado = JSON.parse(getProducto);
+const mesesParseado = JSON.parse(getMeses);
+
+console.log(typeof productoParseado); //object
+console.log(typeof mesesParseado); //object
+console.log(productoParseado); // print objecto
+console.log(mesesParseado); // print array
