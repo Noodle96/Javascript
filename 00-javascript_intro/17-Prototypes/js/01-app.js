c457c29b5133754735d1c7d3741cd9b202eab5a5
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-04 16:36:04 -0500

    Object literal vs Object constructor

---
+++ 00-javascript_intro/17-Prototypes/js/01-app.js
@@ -0,0 +1,16 @@
+//OBJECT LITERAL
+const vanish = {
+    nombre: 'Vanish x750',
+    precio: 7.20
+}
+console.log(vanish);
+
+function Producto(nombre,precio){
+    this.nombre = nombre;
+    this.precio = precio;
+}
+
+detergente = new Producto('Ariel x1kg',6.3);
+console.log(detergente);
+
+//OBJECT CONSTRUCTOR
