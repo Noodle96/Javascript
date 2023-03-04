2ab94e9ef4ba4276066eb7cb143a5ffb9d11b778
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-04 16:00:36 -0500

    object Date js

---
+++ 00-javascript_intro/16-Fechas/js/01-app.js
@@ -0,0 +1,12 @@
+const diaHoy = new Date();
+
+
+let valor;
+valor = diaHoy;
+valor = diaHoy.getFullYear();
+valor = diaHoy.getMonth(); // 0-1-2-3-4-5-....
+valor = diaHoy.getHours();
+valor = diaHoy.getMinutes();
+valor = diaHoy.getTime();
+
+console.log(valor);
