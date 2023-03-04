17670b0722d6ac3897fd004b9821b26504aa37a8
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-04 16:10:54 -0500

    moment  javascript library

--- 00-javascript_intro/16-Fechas/js/01-app.js
+++ 00-javascript_intro/16-Fechas/js/01-app.js
@@ -1,12 +1,17 @@
 const diaHoy = new Date();
 
+// let valor;
+// valor = diaHoy;
+// valor = diaHoy.getFullYear();
+// valor = diaHoy.getMonth(); // 0-1-2-3-4-5-....
+// valor = diaHoy.getHours();
+// valor = diaHoy.getMinutes();
+// valor = diaHoy.getTime();
+// valor = diaHoy;  
+// console.log(valor);
 
-let valor;
-valor = diaHoy;
-valor = diaHoy.getFullYear();
-valor = diaHoy.getMonth(); // 0-1-2-3-4-5-....
-valor = diaHoy.getHours();
-valor = diaHoy.getMinutes();
-valor = diaHoy.getTime();
+moment.locale('es');
+console.log(moment().format('MMMM Do Y h:mm:ss a') );
+console.log(moment().format('LLLL'));
 
-console.log(valor);
+console.log(moment().add(3,'days').calendar());
