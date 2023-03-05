a40782fcd86381968be485d888f2dd0a2c326531
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-04 23:34:06 -0500

    Herencia en prototypes

--- 00-javascript_intro/17-Prototypes/js/02-app.js
+++ 00-javascript_intro/17-Prototypes/js/02-app.js
@@ -1,7 +1,9 @@
+//EN LAS FUNCIONES DE PROTOTYPES UTILIZAR  FUNCTIONS AND NOT ARROW FUNCTIONS
 function Cliente(name, saldo){
     this.name = name;
     this.saldo = saldo;
 }
+
 
 Cliente.prototype.tipoCliente = function(){
     let tipo;
@@ -23,7 +25,27 @@ Cliente.prototype.retirarSaldo = function(montoRetirar){
 }
 
 const russell = new Cliente('Russell',21000);
+console.log(russell);
 console.log(russell.tipoCliente());
 console.log(russell.nombreClienteSaldo());
 russell.retirarSaldo(15000);
 console.log(russell.nombreClienteSaldo());
+
+console.log('Herencia en prototypes');
+function Persona(name,saldo,telefono){
+    Cliente.call(this,name,saldo);
+    this.telefono = telefono;
+}
+// HEREDANDO LAS FUNCIONES DE CLIENTE A PERSONA
+Persona.prototype = Object.create(Cliente.prototype);
+// CON EL CONSTRUCTOR
+Persona.prototype.constructor = Cliente;
+
+const jorge = new Persona('Jorge',14000,999999999);
+console.log(jorge.tipoCliente());
+
+Persona.prototype.mostrarTelefono = function(){
+    return  `El telefono de ${this.name} es ${this.telefono}`;
+}
+
+console.log(jorge.mostrarTelefono());
