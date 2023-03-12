c30fdf624a2508d29ff70c1a7171744e3cbabf63
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-11 23:14:49 -0500

    POO - funciones miembro de una clase

--- 00-javascript_intro/18-ProgramacionOrientadaObjetos/js/01-app.js
+++ 00-javascript_intro/18-ProgramacionOrientadaObjetos/js/01-app.js
@@ -4,16 +4,27 @@ class Cliente{
         this.name = name;
         this.saldo = saldo;
     }
+    converToString(){
+        return `El nombre es ${this.name} y su saldo es ${this.saldo}`;
+    }
+    static darBienvenida(){
+        // return `Welcome ${this.name}`; //welcome Cliente
+        return `Welcome`; // Welcome
+
+    }
 }
 const pedro = new Cliente('Pedro', 350);
-console.log(pedro);
-
+console.log(pedro.converToString());
+console.log(Cliente.darBienvenida());
 // CLASS EXPRESION
 const Cliente2 = class{
     constructor(name, saldo){
         this.name = name;
         this.saldo = saldo;
     }
+    converToString(){
+        return `El nombre es ${this.name} y su saldo es ${this.saldo}`;
+    }
 }
 const juan = new Cliente2('Juan', 650);
-console.log(juan);
+console.log(juan.converToString());
