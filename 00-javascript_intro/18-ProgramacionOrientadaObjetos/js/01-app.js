983dc11e3eb59718abe5096620337e4af9225d6d
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-11 23:25:37 -0500

    POO - herecia

--- 00-javascript_intro/18-ProgramacionOrientadaObjetos/js/01-app.js
+++ 00-javascript_intro/18-ProgramacionOrientadaObjetos/js/01-app.js
@@ -9,22 +9,41 @@ class Cliente{
     }
     static darBienvenida(){
         // return `Welcome ${this.name}`; //welcome Cliente
-        return `Welcome`; // Welcome
+        return `Welcome Cliente`; // Welcome
+    }
+}
 
+class Empresa extends Cliente{
+    constructor(name,saldo,telefono,categoria){
+        super(name,saldo);
+        this.telefono = telefono;
+        this.categoria = categoria;
+    }
+    static darBienvenida(){ // REESCRIBIENDO EL METODO DE CLIENTE
+        return `Welcome Empresa`; // Welcome
     }
 }
+
+
+
 const pedro = new Cliente('Pedro', 350);
+console.log(pedro);
 console.log(pedro.converToString());
-console.log(Cliente.darBienvenida());
+console.log(Cliente.darBienvenida()); // welcome Cliente
+
+const empresa1 = new Empresa('TechIA',5000,99999999,'Tech');
+console.log(empresa1);
+console.log(Empresa.darBienvenida()); //Welcoe empresa
+
 // CLASS EXPRESION
-const Cliente2 = class{
-    constructor(name, saldo){
-        this.name = name;
-        this.saldo = saldo;
-    }
-    converToString(){
-        return `El nombre es ${this.name} y su saldo es ${this.saldo}`;
-    }
-}
-const juan = new Cliente2('Juan', 650);
-console.log(juan.converToString());
+// const Cliente2 = class{
+//     constructor(name, saldo){
+//         this.name = name;
+//         this.saldo = saldo;
+//     }
+//     converToString(){
+//         return `El nombre es ${this.name} y su saldo es ${this.saldo}`;
+//     }
+// }
+// const juan = new Cliente2('Juan', 650);
+// console.log(juan.converToString());
