928c0b9881a4df11ff3dfc4bd9a957335f3c1737
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-25 23:26:18 -0500

    Add class Presupuesto

--- 07-PROYECTO-GastoPresupuestoSemanalConClases/js/app.js
+++ 07-PROYECTO-GastoPresupuestoSemanalConClases/js/app.js
@@ -1,6 +1,7 @@
 // VARIABLES Y SELECTORES 
 const formulario = document.querySelector('#agregar-gasto');
 const gastoListado = document.querySelector('#gastos ul');
+let presupuesto;
 
 // EVENTOS 
 eventListeners();
@@ -8,18 +9,27 @@ function eventListeners(){
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
 }
 
-console.log('welxome to clases');
 
 
 // CLASES
-
+class Presupuesto{
+    constructor(presupuesto){
+        this.presupuesto= presupuesto;
+        this.restante = presupuesto;
+        this.gastos = [];
+    }
+}
+class UI{}
+const ui = new UI();
 
 
 // FUNCIONES 
 function preguntarPresupuesto(){
     const presupuestoUsuario = prompt('¿Cuál es tu presupuesto?');
-    console.log(presupuestoUsuario);
     if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || Number(presupuestoUsuario) <= 0){
         window.location.reload();
-    }  
+    }
+    // PRESUPUESTO VALIDO
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
 }
