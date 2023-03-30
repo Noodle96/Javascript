984a710c883212ef3f838ad8cead1848c1e5cc6f
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-30 17:56:07 -0500

    Validacion de formulario

--- 08-PROYECTO-AdministrarCitas/js/app.js
+++ 08-PROYECTO-AdministrarCitas/js/app.js
@@ -11,16 +11,41 @@ const contenedorCitas = document.querySelector('#citas');
 //LISTENERS
 eventListeners();
 function eventListeners(){
-    mascotaInput.addEventListener('change', datosCita);
-    propietarioInput.addEventListener('change', datosCita);
-    telefonoInput.addEventListener('change', datosCita);
-    fechaInput.addEventListener('change', datosCita);
-    horaInput.addEventListener('change', datosCita);
-    sintomasInput.addEventListener('change', datosCita);
-
+    mascotaInput.addEventListener('change', createUpdateObjCita);
+    propietarioInput.addEventListener('change', createUpdateObjCita);
+    telefonoInput.addEventListener('change', createUpdateObjCita);
+    fechaInput.addEventListener('change', createUpdateObjCita);
+    horaInput.addEventListener('change', createUpdateObjCita);
+    sintomasInput.addEventListener('change', createUpdateObjCita);
+    formulario.addEventListener('submit', validarAgregarCita);
 }
 
-//OBJETOS
+////////////////////////////////////////////////////////////////////
+/*--------------------------CREATE CLASES-------------------------*/
+class Citas{
+    constructor(){
+        this.citas = [];
+    }
+}
+class UI{
+    mostrarAlerta(mensaje, tipoMensaje){
+        const divError = document.createElement('div');
+        divError.classList.add('text-center', 'alert', 'd-block','col-12', tipoMensaje);
+        divError.textContent = mensaje;
+        document.querySelector('.agregar-cita').insertBefore(divError,formulario);
+        //quitar alerta despues de 4 segundos
+        setTimeout(() => {
+            divError.remove();
+        }, 4000);
+    }
+}
+/////////////////////////////////END////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////
+/*-----------INSTANCIA DE LAS CLASES - CREAR OBJETO---------------*/
+const administrarCitas = new Citas();
+const ui = new UI();
 const citaObj = {
     mascota: '',
     propietario: '',
@@ -29,9 +54,27 @@ const citaObj = {
     hora: '',
     sintomas: '',
 }
+/////////////////////////////////END////////////////////////////////
 
-//FUNCIONES
-function datosCita(e){
-    citaObj[e.target.name] = e.target.value;
+
+////////////////////////////////////////////////////////////////////
+/*--------------------------FUNCIONES-----------------------------*/
+//lee el contenido del campo y actualiza el objeto "citaObj"
+function createUpdateObjCita(e){
+    citaObj[e.target.name] = e.target.value.trim();
     console.log(citaObj);
 }
+//valida y agrega la cita a la lista del miembro de la clase Cita
+function validarAgregarCita(e){
+    e.preventDefault();
+    // extraer la informacion del citaObj
+    const { mascota, propietario,telefono,fecha, hora,sintomas} =  citaObj;
+    //validar cada atributo del objeto citaObj
+    if(mascota === '' || propietario === '' || telefono === '' ||
+         fecha === '' || hora === '' || sintomas === ''){
+            ui.mostrarAlerta('Todos los campos son Obligatorios', 'alert-danger');
+            return;
+    }
+    //creando una nueva cita
+}
+/////////////////////////////////END////////////////////////////////
