e065ebff7e7e31a840d321809381e7d3ab953e54
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-30 18:36:44 -0500

    Guardar las citas al miembro de la clase

--- 08-PROYECTO-AdministrarCitas/js/app.js
+++ 08-PROYECTO-AdministrarCitas/js/app.js
@@ -22,9 +22,24 @@ function eventListeners(){
 
 ////////////////////////////////////////////////////////////////////
 /*--------------------------CREATE CLASES-------------------------*/
+class Caracteristicas{
+    constructor(mas,pro,tel,fec,hor,sin,id){
+        this.mascota = mas;
+        this.propietario = pro;
+        this.telefono = tel;
+        this.fecha = fec;
+        this.hora = hor;
+        this.sintomas = sin;
+        this.id = id;
+    }
+}
 class Citas{
     constructor(){
         this.citas = [];
+    }
+    addCita(cita){
+        this.citas = [...this.citas,cita];
+        console.log(this.citas);
     }
 }
 class UI{
@@ -62,7 +77,7 @@ const citaObj = {
 //lee el contenido del campo y actualiza el objeto "citaObj"
 function createUpdateObjCita(e){
     citaObj[e.target.name] = e.target.value.trim();
-    console.log(citaObj);
+    // console.log(citaObj);
 }
 //valida y agrega la cita a la lista del miembro de la clase Cita
 function validarAgregarCita(e){
@@ -75,6 +90,28 @@ function validarAgregarCita(e){
             ui.mostrarAlerta('Todos los campos son Obligatorios', 'alert-danger');
             return;
     }
+    //generar un id unico
+    citaObj.id = Date.now();
     //creando una nueva cita
+    // const nuevaCita = new Caracteristicas(mascota,propietario,telefono,fecha,hora,sintomas,Date.now());
+    // administrarCitas.addCita(nuevaCita);
+
+    //se pasa una copia cuando es 'OBJETO GLOBAL'
+    administrarCitas.addCita({...citaObj});
+    ui.mostrarAlerta('Cita Agregada Correctamente','alert-success');
+
+    //resetearFormulario
+    formulario.reset();
+
+    reiniciarObjetoGlobal();
+}
+//reinicia el objeto global
+function reiniciarObjetoGlobal(){
+    citaObj.mascota = '';
+    citaObj.propietario= '';
+    citaObj.telefono= '';
+    citaObj.fecha= '';
+    citaObj.hora= '';
+    citaObj.sintomas= '';
 }
 /////////////////////////////////END////////////////////////////////
